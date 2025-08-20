8f515f6aa53f7082ebd4a1a4810253bef82acee0
andrewzshen <andrewshenrocks@gmail.com>
2025-08-19 21:29:04 -0700

    fixes to solution

--- problems/zen/solution.py
+++ problems/zen/solution.py
@@ -3,18 +3,21 @@ import codecs
 import re
 from collections import defaultdict
 
-def main():
+def parse_zen():
     zen = codecs.decode(this.s, "rot13")
     positions = defaultdict(list)
     
-    regex = re.compile(r"[a-zA-Z]+(?:'[a-zA-z]+)?")
+    regex = re.compile(r"[a-zA-Z]+(?:'[a-zA-Z]+)?")
 
     for row, line in enumerate(zen.splitlines(), start=1):
         for col, match in enumerate(regex.finditer(line), start=1):
             word = match.group(0)
             positions[word].append((row, col))
     
-    print(dict(positions))
+    return dict(positions)
+
+def main():
+    print(parse_zen())
 
 if __name__ == "__main__":
     main()
